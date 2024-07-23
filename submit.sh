5fe20e74517d63aa91188ad3f5f43786871002b9
Lasse <bxc755@esrumhead01fl.unicph.domain>
2024-07-23 14:11:38 +0200

    clean start

--- submit.sh
+++
@@ -1,9 +0,0 @@
-rm snakemake_output/snakemake.oe
-dir="/maps/projects/rasmussen/scratch/ptracker/ptracker/snakemake_output"
-
-snakemake -n --rerun-incomplete  --snakefile snakefile.smk \
---jobs 2 --latency-wait 60 \
---cluster "sbatch  --output={log.o}.cluster --error={log.e}.cluster --time=00:00:10 --job-name {rule}  --cpus-per-task {threads} --mem {resources.mem_gb}G "
-
-# --cluster "sbatch --time=00:00:10 --output $dir/snakemake_output/%j.o --error $dir/snakemake_output/%j.o --job-name {rule} --cpus-per-task {threads} --mem {resources.mem_gb}G" 
-#--error=$dir/%x.out 
